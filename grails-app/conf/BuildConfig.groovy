73f776c59d64f84c0e16a7bbda87e3f09a1411f9
Martin Minka <martin.minka@gmail.com>
2013-02-15 23:51:14 +0100

    jetty doesn't seam to be needed

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,20 +17,22 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
+
 grails.project.dependency.resolution = {
     inherits 'global'
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {        
+    repositories {
         mavenCentral()
     }
     dependencies {
         def cometdVer = '2.5.1'
-        def jettyVer = '7.6.3.v20120416'
+        //def jettyVer = '7.6.3.v20120416'
         compile(group: 'org.cometd.java', name: 'cometd-java-server', version: cometdVer) {
             excludes 'servlet-api'
         }
         compile group: 'org.cometd.java', name: 'bayeux-api', version: cometdVer
-        runtime group: 'org.eclipse.jetty', name: 'jetty-servlets', version: jettyVer
+        //runtime group: 'org.eclipse.jetty', name: 'jetty-servlets', version: jettyVer
         test group: 'org.cometd.java', name: 'cometd-java-client', version: cometdVer
     }
 }
+
